666b2550df60ec2281ba8765ebd055ede9537b9e
Napster1010 <aditya.ndps@gmail.com>
2023-01-17 00:45:31 +0530

    ex3 solution
    Signed-off-by: Napster1010 <aditya.ndps@gmail.com>

---
+++ ex3.js
@@ -0,0 +1,47 @@
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+	return new Promise(function(resolve, reject) {
+		fakeAjax(file, resolve);
+	});
+}
+
+var prom1 = getFile("file1");
+var prom2 = getFile("file2");
+var prom3 = getFile("file3");
+
+
+prom1
+.then(text1 => {
+	output(text1);
+	return prom2;
+})
+.then(text2 => {
+	output(text2);
+	return prom3;
+})
+.then(text3 => {
+	output(text3);
+	output("Complete!");
+});
+
+
